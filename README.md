3808467636d6cfa651ac0bfc98598e393c3382c3
AlanThinking <1512584131@qq.com>
2026-01-25 22:47:46 +0800

    add backend-mall

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # E-commerce-Shopping-Mall-System
 基于Spring Boot+Vue的3c电子购物商城
+
+
+
+修改提交
---
+++ backend-mall/src/main/java/com/thinking/backendmall/BackendMallApplication.java
@@ -0,0 +1,13 @@
+package com.thinking.backendmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendMallApplication.class, args);
+    }
+
+}
